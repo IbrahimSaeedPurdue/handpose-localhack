127077a80a3f973900c60c55dc442ff8539e0659
IbrahimSaeedPurdue <54752803+IbrahimSaeedPurdue@users.noreply.github.com>
2021-01-13 19:49:58 -0400

    drawing landmarks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { useRef } from 'react';
 
-//for web camera
+// for web camera
 import Webcam from 'react-webcam';
 
-//for tensorflowjs and handpose related
+// for tensorflowjs and handpose related
 import * as tf from '@tensorflow/tfjs-core';
-//import '@tensorflow/tfjs-backend-webgl';
-//import * as handpose from "@tensorflow-models/handpose";
+// import '@tensorflow/tfjs-backend-webgl';
+// import * as handpose from "@tensorflow-models/handpose";
 
-//for mediapipe handpose related
-import {Camera} from '@mediapipe/camera_utils/camera_utils';
-import {Hands} from '@mediapipe/hands/hands';
-import {drawConnectors, drawLandmarks} from '@mediapipe/drawing_utils/drawing_utils';
+// for mediapipe handpose related
+import { Camera } from '@mediapipe/camera_utils/camera_utils';
+import { Hands } from '@mediapipe/hands/hands';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils/drawing_utils';
 
+// TODO: import custom model
 
-//TODO: import custom model
+// for canvas drawing on top of webcam
+import { drawHand, drawHandMP, drawRed } from './drawBoundary';
 
-//for canvas drawing on top of webcam
-import {drawHand, drawHandMP, drawRed} from "./drawBoundary";
-
-//require('@tensorflow/tfjs-backend-webgl');
+// require('@tensorflow/tfjs-backend-webgl');
 
 /**
  * code referencing:
  * "Real Time AI Hand Pose estimation with Javascript, Tensorflow.JS and React.JS"
  * by Nicholas Renotte
- * & MediaPipe hand detection 
+ * & MediaPipe hand detection
  */
 
 const App = () => {
@@ -38,7 +37,7 @@ const App = () => {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  //using the tensorflow implementation of handpose
+  // using the tensorflow implementation of handpose
   /*
   //load the handpose model
   const runHandpose = async () => {
@@ -80,133 +79,132 @@ const App = () => {
     }
   }
 
-  runHandpose();*/
+  runHandpose(); */
 
-  
   const onResults = (results) => {
-    if (results !== null){
-      console.log(results);
-    }
+    console.log(results);
+    // if (results !== null) {
+    //   console.log(results);
+    // }
     /*
     // Hide the spinner.
     document.body.classList.add('loaded');
 
     // Update the frame rate.
-    fpsControl.tick();*/
+    fpsControl.tick(); */
 
-    const canvasCtx = canvasRef.current.getContext("2d");
+    const canvasCtx = canvasRef.current.getContext('2d');
     const canvasElement = canvasRef.current;
 
     // Draw the overlays.
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    //canvasCtx.drawImage(
-        //results.image, 0, 0, canvasElement.width, canvasElement.height);
+    // canvasCtx.drawImage(
+    // results.image, 0, 0, canvasElement.width, canvasElement.height);
     if (results.multiHandLandmarks && results.multiHandedness) {
       for (let index = 0; index < results.multiHandLandmarks.length; index++) {
-        
         const classification = results.multiHandedness[index];
         const isRightHand = classification.label === 'Right';
         const landmarks = results.multiHandLandmarks[index];
-        
-        //draw custom hands using the drawboundary methods
+
+        // draw custom hands using the drawboundary methods
         drawHandMP(results.image, classification, landmarks, canvasCtx);
 
-        //draw hand using utils
-        /*
-        drawConnectors(
-            canvasCtx, landmarks, HAND_CONNECTIONS,
-            {color: isRightHand ? '#00FF00' : '#FF0000'});
-        drawLandmarks(canvasCtx, landmarks, {
-          color: isRightHand ? '#00FF00' : '#FF0000',
-          fillColor: isRightHand ? '#FF0000' : '#00FF00'
-        });*/
+        // draw hand using utils
+
+        // drawConnectors(
+        //   canvasCtx, landmarks, HAND_CONNECTIONS,
+        //   { color: isRightHand ? '#00FF00' : '#FF0000' });
+        // drawLandmarks(canvasCtx, landmarks, {
+        //   color: isRightHand ? '#00FF00' : '#FF0000',
+        //   fillColor: isRightHand ? '#FF0000' : '#00FF00'
+        // });
       }
-    }
-    else{
+    } else {
+      // red boundary to indicate no hand in view
       drawRed(canvasCtx);
     }
     canvasCtx.restore();
-  }
+  };
 
-  var cam = null;
+  let cam = null;
 
   const setupComponents = async () => {
-    
-    //initialize ML model
-    const hands = new Hands({locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-    }});
+    // initialize ML model
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      }
+    });
     hands.setOptions({
       maxNumHands: 1,
       minDetectionConfidence: 0.5,
       minTrackingConfidence: 0.5
     });
-  
+
     hands.onResults(onResults);
 
     setInterval(() => {
       sendPicture(hands);
     }, 100);
-  }
+  };
 
   const sendPicture = async (hands) => {
-    //validate videofeed
+    // validate videofeed
     if (
-      typeof webcamRef.current != "undefined"
-      && webcamRef.current !== null
-      && webcamRef.current.video.readyState === 4
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
     ) {
       const video = webcamRef.current.video;
       const vw = video.width;
       const vh = video.height;
-      //force parameters to set
+      // force parameters to set
       video.width = vw;
       video.height = vh;
 
       canvasRef.current.width = vw;
       canvasRef.current.height = vh;
 
-      if (cam === null){
-        console.log("setup camera");
+      if (cam === null) {
+        console.log('setup camera');
         cam = new Camera(video, {
           onFrame: async () => {
-            await hands.send({image: video});
+            await hands.send({ image: video });
           },
           width: 720,
           height: 540
         });
         cam.start();
       }
-      
     }
-  }
+  };
 
   setupComponents();
 
   return (
-    <div className="App">
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          screenshotFormat='image/jpeg'
-          width = "720"
-          height = "540"
-          videoConstraints={videoConstraints}
-          style={{
-            position: "absolute",
-            zIndex: 9
-          }}
-        />
-        <canvas 
-          ref = {canvasRef}
-          style={{
-            position: "absolute",
-            height: 540,
-            width: 720,
-            zIndex: 9
-          }}
-        />
+    <div className='App'>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat='image/jpeg'
+        width='720'
+        height='540'
+        videoConstraints={videoConstraints}
+        style={{
+          position: 'absolute',
+          zIndex: 9
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          height: 540,
+          width: 720,
+          zIndex: 9
+        }}
+      />
     </div>
   );
 };
--- src/drawBoundary.js
+++ src/drawBoundary.js
@@ -1,81 +1,142 @@
+import { convertCompilerOptionsFromJson } from 'typescript';
+
 function lerp (v, w, t) {
-    return v + (w - v) * t;
+  return v + (w - v) * t;
 }
 
-//draw green boundary around the hand
+// draw green boundary around the hand
 export const drawHand = (prediction, ctx) => {
+  // ctx.clearRect(0, 0, w, h);
+  ctx.fillStyle = 'green';
+  ctx.strokeStyle = 'rgba(171, 235, 52, 1)';
+  // console.log(ctx);
+  // console.log(prediction);
 
-    //ctx.clearRect(0, 0, w, h);
-    ctx.fillStyle = "green";
-    ctx.strokeStyle = 'rgba(171, 235, 52, 1)';
-    //console.log(ctx);
-    //console.log(prediction);
+  if (prediction.length > 0) {
+    prediction.forEach(hand => {
+      const x1 = hand.boundingBox.topLeft[0];
+      const x2 = hand.boundingBox.bottomRight[0];
+      const y1 = hand.boundingBox.topLeft[1];
+      const y2 = hand.boundingBox.bottomRight[1];
 
-    if (prediction.length > 0){
-        prediction.forEach(hand => {
+      ctx.lineWidth = lerp(0, 20, hand.handInViewConfidence);
+      // console.log('confidence: ' + hand.handInViewConfidence);
+      ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+    });
+  }
+};
 
-            let x1 = hand.boundingBox.topLeft[0];
-            let x2 = hand.boundingBox.bottomRight[0];
-            let y1 = hand.boundingBox.topLeft[1];
-            let y2 = hand.boundingBox.bottomRight[1];
+// draw green boundary around the hand, but using mediapipe's data format
+// export const drawHandMP = (image, classification, marks, ctx) => {
+//   const w = image.width;
+//   const h = image.height;
 
-            ctx.lineWidth = lerp(0, 20, hand.handInViewConfidence);
-            console.log("confidence: " + hand.handInViewConfidence);
-            ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
-        });
-    }
-}
+//   const score = classification.score;
 
-//draw green boundary around the hand, but using mediapipe's data format
+//   ctx.clearRect(0, 0, w, h);
+//   ctx.fillStyle = 'green';
+//   ctx.strokeStyle = 'rgba(171, 235, 52, 1)';
+//   // console.log(ctx);
+//   // console.log(prediction);
+
+//   // find the minimum and maximum boundaries from the landmarks, and use that to get boundary of the hand
+//   if (marks.length > 0) {
+//     let x1 = 100000000; let x2 = -100000000; let y1 = 1000000000; let y2 = -1000000000;
+
+//     for (let i = 0; i < marks.length; i++) {
+//       if (marks[i].x < x1) {
+//         x1 = marks[i].x;
+//       }
+//       if (marks[i].x > x2) {
+//         x2 = marks[i].x;
+//       }
+//       if (marks[i].y < y1) {
+//         y1 = marks[i].y;
+//       }
+//       if (marks[i].y > y2) {
+//         y2 = marks[i].y;
+//       }
+//     }
+
+//     x1 *= w;
+//     x2 *= w;
+//     y1 *= h;
+//     y2 *= h;
+
+//     ctx.lineWidth = lerp(0, 20, score);
+//     // console.log("confidence: " + score);
+//     // console.log(x1, y1, x2, y2);
+//     ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+//   }
+// };
+
 export const drawHandMP = (image, classification, marks, ctx) => {
+  if (marks.length === 0) return;
 
-    let w = image.width;
-    let h = image.height;
+  // all inclusive
+  // 0 - base
+  // thumb 0 - 4
+  // index 5 - 8
+  // middle 9 - 12
+  // ring 13 - 16
+  // pinky 17 - 21
+  const fingers = [
+    { range: [1, 4], color: 'red' },
+    { range: [5, 8], color: 'blue' },
+    { range: [9, 12], color: 'yellow' },
+    { range: [13, 16], color: 'orange' },
+    { range: [17, marks.length - 1], color: 'lime' }
+  ];
 
-    let score = classification.score;
+  fingers.forEach(finger => {
+    for (let i = finger.range[0]; i < finger.range[1]; i++) {
+      const x1 = marks[i].x * image.width;
+      const y1 = marks[i].y * image.height;
 
-    ctx.clearRect(0, 0, w, h);
-    ctx.fillStyle = "green";
-    ctx.strokeStyle = 'rgba(171, 235, 52, 1)';
-    //console.log(ctx);
-    //console.log(prediction);
+      // first dot
+      ctx.beginPath();
+      ctx.arc(x1, y1, 5, 0, 3 * Math.PI);
+      ctx.fillStyle = finger.color;
+      ctx.fill();
 
-    //find the minimum and maximum boundaries from the landmarks, and use that to get boundary of the hand
-    if (marks.length > 0){
+      ctx.moveTo(x1, y1);
 
-        let x1 = 100000000, x2 = -100000000, y1 = 1000000000, y2 = -1000000000;
+      const x2 = marks[i + 1].x * image.width;
+      const y2 = marks[i + 1].y * image.height;
 
-        for (let i = 0; i < marks.length; i++){
-            if (marks[i].x < x1){
-                x1 = marks[i].x;
-            }
-            if (marks[i].x > x2){
-                x2 = marks[i].x;
-            }
-            if (marks[i].y < y1){
-                y1 = marks[i].y;
-            }
-            if (marks[i].y > y2){
-                y2 = marks[i].y;
-            }
-        }
+      // line
+      ctx.lineTo(x2, y2);
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = finger.color;
+      ctx.stroke();
 
-        x1 *= w;
-        x2 *= w;
-        y1 *= h;
-        y2 *= h;
-
-        ctx.lineWidth = lerp(0, 20, score);
-        //console.log("confidence: " + score);
-        console.log(x1, y1, x2, y2);
-        ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+      // second dot
+      ctx.arc(x2, y2, 5, 0, 3 * Math.PI);
+      ctx.fillStyle = finger.color;
+      ctx.fill();
     }
-}
+  });
 
-//draw red boundary around the entire webcam, when no hand is detected
+  // connecting each finger to the base
+  const baseX = marks[0].x * image.width;
+  const baseY = marks[0].y * image.height;
+  fingers.forEach(finger => {
+    // first landmark in finger
+    const x = marks[finger.range[0]].x * image.width;
+    const y = marks[finger.range[0]].y * image.height;
+
+    ctx.moveTo(baseX, baseY);
+    ctx.lineTo(x, y);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+    ctx.stroke();
+  });
+};
+
+// draw red boundary around the entire webcam, when no hand is detected
 export const drawRed = (ctx) => {
-    //console.log("no hand shown " + ctx.width + ", " + ctx.height);
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 10;
-    ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
+  // console.log("no hand shown " + ctx.width + ", " + ctx.height);
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 10;
+  ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+};
